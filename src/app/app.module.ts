6a1aad660675b07648be69e8c5f025686a6cffaa
richvik <richviknov@gmail.com>
2017-10-14 00:19:51 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,13 @@ import { AppComponent } from './app.component';
 import {GlobalService} from './Service/global-service.service';
 
 import {PagesModule} from './Pages/pages.module'
-
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 // pages
 
 @NgModule({
   imports: [BrowserModule, PagesModule, FormsModule, HttpModule, AppRoutingModule],
   declarations: [AppComponent],
-  providers: [GlobalService],
+  providers: [GlobalService,{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
